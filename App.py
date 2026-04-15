be8ef3ebcb148a27214abb96fbe6b03e2201a694
lavanyamahadevanayak-cpu <lavanyamahadevanayak@gmail.com>
2026-04-15 11:14:07 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from groq import Groq
+
+st.set_page_config("pragyanAI Context Generator".layout="width")
+st.title("pragyanAI -context Generator")
+st.image("Screenshot 2025-12-24 082406.png")
+client=Groq(api_key=st.secrets["GROQ_API_KEY"])
+product = st.text_input("Product")
+audience = st.text_input("Audience")
+if st.button("Generate Content"):
+  prompt = f"Write marketing content for {product} targeting {audience}."
+  response = client.chat.chat.completions.create{
+  model ="llama-3.3-70b-versatile",
+  messages=[{"role": "user", "content": prompt}]
+  )
+  st.session_stste.text = response.choices[0.message.content
+  text =response.choice[0.message.content
+  st.write(text)
+  if "text" in st.session_state:
+      content = st.text_area("Generated Content", st.ession_state.text, height=300)
+      st.download_button(
+        label="Doumload as TXT",
+        data=content,
+        file_name="marketing_copy.txt",
+        mime="text/plain"
+      )
+      else:
+          pt.info("Generate content first")
+
+
+  
